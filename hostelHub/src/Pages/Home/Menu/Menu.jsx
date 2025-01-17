9903681309946b30bbe62c500a1f3de357652817
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-17 22:41:47 +0600

    menu card design updated

--- hostelHub/src/Pages/Home/Menu/Menu.jsx
+++ hostelHub/src/Pages/Home/Menu/Menu.jsx
@@ -13,21 +13,21 @@ const Menu = () => {
     },
     {
       image:
-        "https://merida.themepul.com/wp-content/uploads/2024/08/Rectangle-320.jpg",
+        "https://img.freepik.com/free-photo/food-mix-salad-noodles-grillea-chicken-garlic-greena-top-view_141793-15488.jpg?semt=ais_incoming",
       mealName: "Chicken Parmesan",
       description: "A delightful classic with a crispy touch…",
       price: "$25.0",
     },
     {
       image:
-        "https://merida.themepul.com/wp-content/uploads/2024/08/Rectangle-321.jpg",
+        "https://img.freepik.com/free-photo/grilled-chicken-steak-vegetables-dark-background_1150-45259.jpg?semt=ais_incoming",
       mealName: "Grilled Salmon",
       description: "Freshly caught salmon grilled to perfection…",
       price: "$38.0",
     },
     {
       image:
-        "https://merida.themepul.com/wp-content/uploads/2024/08/Rectangle-322.jpg",
+        "https://img.freepik.com/free-photo/pieces-chicken-fillet-with-mushrooms-stewed-tomato-sauce-with-boiled-broccoli-rice-proper-nutrition-healthy-lifestyle-dietetic-menu-top-view_2829-20015.jpg?semt=ais_incoming",
       mealName: "Beef Wellington",
       description:
         "Tender beef wrapped in flaky pastry for the ultimate treat…",
@@ -35,14 +35,14 @@ const Menu = () => {
     },
     {
       image:
-        "https://merida.themepul.com/wp-content/uploads/2024/08/Rectangle-323.jpg",
+        "https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?semt=ais_incoming",
       mealName: "Caesar Salad",
       description: "Crisp romaine lettuce tossed with our signature dressing…",
       price: "$18.0",
     },
     {
       image:
-        "https://merida.themepul.com/wp-content/uploads/2024/08/Rectangle-324.jpg",
+        "https://img.freepik.com/free-photo/side-view-christmas-food-appetizing-christmas-dish-with-seeds-pomegranate-plate-grey-table_140725-111290.jpg?semt=ais_incoming",
       mealName: "Spaghetti Bolognese",
       description: "A traditional Italian favorite with rich meat sauce…",
       price: "$22.0",
@@ -57,38 +57,6 @@ const Menu = () => {
           <MenuCard key={index} meal={meal} />
         ))}
       </div>
-      {/* <div className="flex flex-col md:flex-row gap-4 my-8">
-        <div className="w-full md:w-1/2">
-          <div
-            className="py-3 mb-8 pl-12"
-            style={{ backgroundImage: `url(${menuBg})` }}
-          >
-            <h2 className="text-3xl font-semibold">Breakfast Menu</h2>
-          </div>
-          <div className="space-y-8">
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-          </div>
-        </div>
-        <div className="w-full md:w-1/2">
-          <div
-            className="py-3 mb-8 pl-12"
-            style={{ backgroundImage: `url(${menuBg})` }}
-          >
-            <h2 className="text-3xl font-semibold">Lunch Menu</h2>
-          </div>
-          <div className="space-y-8">
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-            <MenuCard />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
--- hostelHub/src/Pages/Home/Menu/MenuCard.jsx
+++ hostelHub/src/Pages/Home/Menu/MenuCard.jsx
@@ -2,26 +2,22 @@ import { IoCartOutline } from "react-icons/io5";
 
 const MenuCard = ({ meal }) => {
   return (
-    <div className="flex justify-around group">
-      <div className="relative ">
+    <div className="flex justify-between gap-4 group border-2 border-gray-200 shadow-lg rounded-lg p-2">
+      <div className="relative">
         <img className="rounded-full size-20" src={meal?.image} alt="" />
-        <div className="absolute size-16 bg-themeColor group-hover:flex hidden  z-10 rounded-full top-2 left-2">
-          <div className="translate-x-1/2 translate-y-1/2 text-3xl text-white ">
+        <div className="absolute size-20 bg-themeColor/40 group-hover:flex hidden duration-300 transition-all z-10 rounded-full top-0 left-0">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-white font-bold">
             <IoCartOutline />
           </div>
         </div>
       </div>
-      <div className="">
-        <div className="flex justify-between items-center">
-          <h2 className="text-2xl font-semibold">{meal?.mealName}</h2>
-          <hr />
-        </div>
+      <div className="min-w-fit flex flex-grow flex-col justify-center">
+        <h2 className="text-2xl font-semibold">{meal?.mealName}</h2>
+
         <p className="font-bold text-gray-500">{meal?.description}</p>
       </div>
-      <div>
-        <h2 className="text-2xl items-center flex text-themeColor">
-          {meal?.price}
-        </h2>
+      <div className="text-center flex justify-center items-center pr-3">
+        <h2 className="text-2xl text-themeColor">{meal?.price}</h2>
       </div>
     </div>
   );
